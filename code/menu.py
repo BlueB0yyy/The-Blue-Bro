81c79dc3735bc0cf78b2a04fa53056af28a814b6
BlueB0yyy <thi2mil@gmail.com>
2025-08-15 23:40:07 -0300

    1st commit

---
+++ code/menu.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import pygame
+from pygame import Surface, Rect
+from pygame.font import Font
+
+from code.const import WINDOW_WIDTH, COLOR_BLUE
+
+
+class Menu:
+    def __init__(self, window):
+        self.window = window
+        self.surf = pygame.image.load('./asset/Level/Background/Day/Background.png').convert_alpha()
+        self.rect = self.surf.get_rect(left=0, top=0)
+
+    def run(self, ):
+        option = 0
+        while True:
+            # metodo para desenhar a imagem (vem da superfície e DESENHA no retângulo)
+            self.window.blit(source=self.surf, dest=self.rect)
+            # atenção! Deve ser desenhado DEPOIS da tela (senão fica sobreposto)
+            self.text_menu(70, "The Blue", COLOR_BLUE, (WINDOW_WIDTH / 2, 60))
+            self.text_menu(70, "Bro", COLOR_BLUE, (WINDOW_WIDTH / 2, 100))
+            pygame.display.flip()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                    print('Encerrando')
+
+    def text_menu(self, size:int, text:str, color:tuple, pos: tuple):
+        font: Font = pygame.font.SysFont("Comic Sans",size=size)
+        text_surf: Surface = font.render(text, True, color)
+        text_rect: Rect = text_surf.get_rect(center=pos)
+        self.window.blit(source=text_surf, dest=text_rect)
