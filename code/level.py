b8c52f14b67a7ca9973dce349ec9633345ce18f4
Thiago Mikulin <thiago.smikulin@gmail.com>
2025-08-22 13:37:38 -0300

    Update level.py
    * Load the tile_map and entity_list as part of the class creation
    * Smplifying run method (only executes fixed actions and methods)

--- code/level.py
+++ code/level.py
@@ -10,13 +10,13 @@ class Level:
         self.window = window
         self.name = name
         self.entity_list: list[Entity] = []
-        self.entity_list.extend(EntityFactory.load_level(self.name))  #Arquivo .csv carregado em lista)
+        #self.entity_list.extend(EntityFactory.load_level(self.name))  #Arquivo .csv carregado em lista)
+        self.tile_map = EntityFactory.load_level(self.name)
+        self.entity_list.extend(EntityFactory.draw_level(self.tile_map, self.name))
+        print(self.entity_list)
         self.size = None
 
     def run(self, ):
-        tile_map = EntityFactory.load_level(self.name)
-        print(self.entity_list)
-        var = EntityFactory.draw_level(tile_map, self.name)
         while True:
             for ent in self.entity_list:
                 self.window.blit(source=ent.surf, dest=ent.rect)
