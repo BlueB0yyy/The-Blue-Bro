5fc6185793aaa9fd0808d642cbc3feb475d570eb
Thiago Mikulin <thiago.smikulin@gmail.com>
2025-08-22 15:59:44 -0300

    Update entity.py
    Still has to check if it works (new load_frames method)

--- code/entity.py
+++ code/entity.py
@@ -11,8 +11,9 @@ class Entity(ABC):
     def __init__(self, name: str, position: tuple, sprite_set:str):
         self.name = name #nome da entidade
         self.surf = pygame.image.load("./asset/"+name+"/"+sprite_set+".png").convert_alpha() #pre-load da 1ª imagem
-        self.rect = self.surf.get_rect(left=position[0], top=position[1])
-        self.sprite_set = sprite_set
+        self.rect = self.surf.get_rect(left=position[0], top=position[1]) #Onde vai ser desenhado
+        self.sprite_set = sprite_set #Para alteração dependendo do botão pressionado
+        self.num_frames = NUM_FRAMES[self.name][self.sprite_set] #Quantidade de frames de cada imagem (INSERIR NO CONST!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!)
         self.sprite_diff = SPRITE_DIFFERENCE[self.name][self.sprite_set]
         self.sprite_limit = SPRITE_LIMIT[self.name][self.sprite_set]
         self.xy = [SPRITE_COORDINATES[self.name][self.sprite_set]["x"],SPRITE_COORDINATES[self.name][self.sprite_set]["y"] ]
@@ -22,6 +23,13 @@ class Entity(ABC):
         self.sprite = pygame.Surface(self.dimensions)
         self.sprite.set_colorkey((0,0,0))
 
-
+    def load_frames(self):
+        frames = []
+        for i in range (self.num_frames):
+            frame_surface = pygame.Surface(self.dimensions[0], self.dimensions[1])
+            frame_surface.blit(self.surf, (0,0), i*self.dimensions[0], 0, self.dimensions[0],self.dimensions[1])
+            frames.append(frame_surface)
+        return frames
+    
     def walk(self, ):
         pass
