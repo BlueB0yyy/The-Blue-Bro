294d172a5d6fdb546345e41179bc90b95f3e5f87
BlueB0yyy <163598393+BlueB0yyy@users.noreply.github.com>
2025-08-27 08:06:28 -0300

    Atualizar o terrain.py
    Comments for better comprehension of the class (no adjustments necessary, any collisions should be measured by the entityMediator from this point forward)

--- code/terrain.py
+++ code/terrain.py
@@ -7,11 +7,20 @@ from code.const import TILE_SIZE
 
 class Terrain:
     def __init__(self, level, name, seq, position):
+
+        # nome do terreno
         self.name = name
         # print(name)
-        self.surf = pygame.image.load("./asset/" + level + "/" + name + "/"+str(seq)+".png").convert_alpha()  # pre-load da 1ª imagem
+
+        #Imagen do terreno (equivalente a sequência do CSV)
+        self.surf = pygame.image.load("./asset/" + level + "/" + name + "/"+str(seq)+".png").convert_alpha()
+
+        # Escalar por medida do terreno (não é o tamanho original)
         self.surf = pygame.transform.scale(self.surf, (TILE_SIZE,TILE_SIZE))
+
+        # Rect correspondente ao tamanho aumentado da Surface 
         self.rect = self.surf.get_rect(left=position[0], top=position[1])
 
+    # Método de desenho próprio
     def draw(self, window):
         window.blit(self.surf, self.rect)
